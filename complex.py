9345c63764dba7a74d730c29f43eb721dfe0c8db
edhexuanw <liuqingyi20032021@163.com>
2024-05-08 10:48:11 +0900

    add new file

---
+++ complex.py
@@ -0,0 +1,11 @@
+class Complex:
+    def __init__(self,real,imag):
+        self.real = real
+        self.imag = imag
+    def __add__ (self,other):
+        return Complex(self.real + other.real,self.imag + other.imag)
+    def __sub__ (self,other):
+        return Complex(self.real - other.real,self.imag - other.imag)
+    def __mul__ (self,other):
+        real = (self.real * other.real - self.imag * other.imag,)
+        
